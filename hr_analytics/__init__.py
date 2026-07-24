bea63eed57dfc8e5eb744bad27beb2fc1fee6e8e
KhalidKhan541 <khalidkhan46572@gmail.com>
2026-07-25 00:08:56 +0500

    feat: add package init with exports

---
+++ hr_analytics/__init__.py
@@ -0,0 +1,8 @@
+"""HR People Analytics & Attrition Prediction.
+
+A production-grade toolkit for generating, analysing, and modelling
+employee attrition data.
+"""
+
+__version__ = "0.1.0"
+__all__ = ["__version__"]
